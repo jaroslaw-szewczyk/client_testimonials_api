17b2ada053a8ca4cdd667e7ffd7729035858dacc
Jarosław Szewczyk <jaroslaw.szewczyk@tutamail.com>
2025-02-05 15:15:33 +0000

    Build app

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./db');
+const path = require('path');
 
 const app = express();
 
@@ -16,10 +17,17 @@ app.use('/api/seats', seatsRoutes);
 app.use('/api/concerts', concertsRoutes);
 app.use('/api/testimonials', testimonialsRoutes);
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
